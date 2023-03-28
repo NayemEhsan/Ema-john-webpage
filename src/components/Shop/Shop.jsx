ffb961f59e946efc1343553f4dcd0f159743a012
NayemEhsan <naymakon@gmail.com>
2023-03-28 22:37:22 +0600

    added cart and local storage export  done

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -42,8 +42,23 @@ const Shop = () => {
     // local storage close
      
     const handleAddToCart =(product)=>{
+        let newCart = [];
 
-        const newCart =[...cart,product];
+        // const newCart =[...cart,product];
+        // if product doesn't exist in the cart,then set quantity =1
+        // if product exist update quantity by 1
+
+        const exists = cart.find(pd =>pd.id === product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart,product]
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd =>pd.id !== product.id);
+            newCart = [...remaining,exists];
+        }
+
         setCart(newCart);
         addToDb(product.id)
     }
--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -4,17 +4,25 @@ const Cart = ({cart}) => {
 
     // total price
     let totalPrice = 0;
-    let totalShipping = 0;    
+    let totalShipping = 0;   
+    let quantity = 0; 
     for(const product of cart){
-       totalPrice = totalPrice + product.price;
+        // if(product.quantity===0){
+        //     product.quantity =1;
+        // }
+        // product.quantity = product.quantity || 1;
+       totalPrice = totalPrice + product.price * product.quantity;
        totalShipping = totalShipping + product.shipping;
+       quantity = quantity + product.quantity;
     }
     const tax = totalPrice*7/100;
     const grandTotal =totalPrice + totalShipping + tax;
     return (
         <div className='cart'>
-          <h3 className='order'>Order Summury</h3>
-                <p className='order'>Cart-Items:{cart.length}</p>
+          <h6 className='ordered'>Order cart</h6>
+          <br className='break' />
+                
+                <p className='order'>Cart-Items:{quantity}</p>
                 <p className='order'>Total Price:${totalPrice.toFixed(2)}</p>
                 <p className='order'>Total Shipping :{totalShipping.toFixed(2)}</p>
                 <p className='order'>Tax:${tax.toFixed(2)}</p>
